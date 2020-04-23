ed5d139346d612d498ceb8e302159e335bc273e9
Jamy <mubashirjamy@gmail.com>
2020-04-23 14:49:46 +0600

    Hibernate CRUD example

--- src/hibernate/Employee.java
+++ src/hibernate/Employee.java
@@ -1,18 +1,55 @@
 package hibernate;
 
 import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 
 @Entity
+@Table(name = "Employee")
 public class Employee implements Serializable{
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    @Id
+    
+//    @Id
+    @Column(name = "name")
     private String name;
+    
     private double salary;
 
+    @Transient
+    private String Aasas;    
+    
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date joining_date = new Date();
+
+    public Date getJoining_date() {
+        return joining_date;
+    }
+
+    public void setJoining_date(Date joining_date) {
+        this.joining_date = joining_date;
+    }
+    
+
+    public String getAasas() {
+        return Aasas;
+    }
+
+    public void setAasas(String Aasas) {
+        this.Aasas = Aasas;
+    }
+    
     public int getId() {
         return id;
     }
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,23 +1,68 @@
-
 package test;
 
 import hibernate.Employee;
 import hibernate.Utility;
+import java.util.Date;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class Test {
+
     public static void main(String[] args) {
-        Employee emp = new Employee();
-        emp.setId(0);
-        emp.setName("Jamy");
-        emp.setSalary(100000);
+        /*
+        - Create SessionFactory object from hibernate utility class
+        - Create Session object from SessionFactory
+        - Begin transaction from session
+        - Save the target object using session
+        - Get the transaction from session and commit it
+        - Close the session and session factory resources
         
+         */
         SessionFactory sessionFactory = Utility.getSessionFactory();
         Session session = sessionFactory.openSession();
+
+        
+//        for (int i = 211; i <= 220; i++) {
+//            Employee emp = new Employee();
+////            emp.setId(i*7*2);
+//            emp.setName("Noman-"+i*15*2);
+//            emp.setSalary(100000*i);
+//            emp.setJoining_date(new Date());
+//            session.beginTransaction(); //Must begin transaction before saving each object
+//            session.save(emp);
+//            session.getTransaction().commit();
+//        }
+
+        System.out.println("Getting the employee with id 14:");
+        Employee e = (Employee) session.get(Employee.class, 14);
+        System.out.println("Retrieved directly from database: "+e.getName()+" | "+e.getSalary());
+
+        System.out.println("New name is being set for the first time:");
+        e.setName("SumiJamySumi-6450");
+        System.out.println("Session update called:");
+        session.update(e);
+        System.out.println("Transaction starting:");
         session.beginTransaction();
-        session.save(emp);
+        System.out.println("Transaction commiting:");
         session.getTransaction().commit();
+        
+        System.out.println("Getting the employee with id 14:");
+        e = (Employee) session.get(Employee.class, 14);
+        System.out.println("Details after update: " + e.getName() + " | " + e.getSalary());
+        
+        System.out.println("Changing the Name again and salary:");
+        e.setSalary(100000);
+        e.setName("JamySumiJamy-6450");
+        System.out.println("Session update calling again");
+        session.update(e);
+        System.out.println("Transaction starting again:");
+        session.beginTransaction();
+        System.out.println("Transaction commiting again:");
+        session.getTransaction().commit();
+        
+        e = (Employee) session.get(Employee.class, 14);
+        System.out.println("Details after updating where id =14: " + e.getName() + " | " + e.getSalary());
+
         session.close();
         sessionFactory.close();
     }
