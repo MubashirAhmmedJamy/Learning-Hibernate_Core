1b8c5f7c95ea3f13391952bc6884a7a4878008ac
Jamy <mubashirjamy@gmail.com>
2020-04-29 00:00:06 +0600

    Criteria API implemented

---
+++ src/test/CriteriaAPItest.java
@@ -0,0 +1,79 @@
+package test;
+
+import hibernate.Employee;
+import hibernate.EmployeeDetails;
+import hibernate.Utility;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.ProjectionList;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+
+public class CriteriaAPItest {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = Utility.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        
+        System.out.println("Fetching all records::\n\n");
+        
+        Criteria criteria = session.createCriteria(Employee.class);
+        
+        List<Employee> list = criteria.list();
+        
+        for(Employee e: list){
+            System.out.println(e.getName() + " | " + e.getEd().getExperience());
+        }
+        
+        
+        System.out.println("Fetching all records with restriction / condition id is between 4 and 10 inclusive");
+        criteria = session.createCriteria(Employee.class);
+        criteria.add(Restrictions.between("id", 4, 10));
+        
+        list = criteria.list();
+        
+        for(Employee e: list){
+            System.out.println(e.getId()+ " | " +e.getName() + " | " + e.getEd().getExperience());
+        }
+        
+        
+        System.out.println("Fetching with multiple OR Restrictions::\n\n");
+
+        criteria = session.createCriteria(Employee.class);
+        
+        criteria.add(Restrictions.or(Restrictions.like("name", "Noman-63%"),Restrictions.lt("salary", 21200000.0))).add(Restrictions.gt("id", 9));
+        list = criteria.list();
+        
+        for(Employee e: list){
+            System.out.println(e.getId()+ " | " +e.getName() + " | " + e.getEd().getExperience()+ " | " + e.getSalary());
+        }
+        
+        
+        System.out.println("Fetching selected properties with Projections::\n\n");
+
+        criteria = session.createCriteria(Employee.class);
+        
+        ProjectionList projectionList = Projections.projectionList();
+
+        projectionList.add(Projections.property("id").as("id"));
+        projectionList.add(Projections.property("name").as("name"));
+        projectionList.add(Projections.property("salary").as("salary"));
+//      projectionList.add(Projections.property("ed").as("ed"));
+        
+        criteria.setProjection(projectionList);
+        
+        criteria.add(Restrictions.or(Restrictions.like("name", "Noman-63%"),Restrictions.lt("salary", 21200000.0))).add(Restrictions.gt("id", 9));
+        
+        
+        List<Object[]> l = criteria.list();
+        
+        
+        System.out.println(l.get(0).length);
+        
+        l.forEach((e) -> {
+            System.out.println(e[1]+ " | " +e[0] + " | " + e[2]);
+        });        
+    }
+}
